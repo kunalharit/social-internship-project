7a91bdc0da87e7cb15b6e5540b073a7fe1f1912c
Kunal Kushwaha <kunalkushwaha969@gmail.com>
2026-07-24 00:48:21 +0530

    order checkout page prototype

--- script.js
+++ script.js
@@ -31,8 +31,33 @@ function toggleTheme() {
     }
 }
 
-// Keep theme consistent on reload
 if (localStorage.getItem('theme') === 'dark') {
     document.body.setAttribute('data-theme', 'dark');
     document.querySelector('.theme-toggle').innerText = '☀️ Light Mode';
 }
+
+// --- GOOGLE SHEET FORM SUBMISSION LOGIC ---
+// Dost ko yahan apni Google Apps Script ki URL dalni hogi baad mein
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwhWHdB7OMIZPJDxBJrW0a35Gk0DlfxpwvqpU9_Jkb8uRKz5ZHtT95huT7BEdG9JzADJg/exec'; 
+const form = document.forms['google-sheet-form'];
+const btn = document.getElementById('submitBtn');
+const msg = document.getElementById('formMsg');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    btn.innerText = "Sending...";
+    
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.style.display = "block";
+            btn.innerText = "Send Inquiry";
+            form.reset();
+            setTimeout(function(){
+                msg.style.display = "none";
+            }, 5000); // 5 second baad message gayab
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            btn.innerText = "Error! Try Again.";
+        });
+});
