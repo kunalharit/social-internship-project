43a9f7f1a1bfe9a16ca75b582664873d2427571c
Kunal Kushwaha <kunalkushwaha969@gmail.com>
2026-07-24 16:03:42 +0000

    Jul 24, 2026, 9:33 PM

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function fetchInventory() {
 }
 
 // --- GOOGLE SHEET FORM SUBMISSION LOGIC ---
-const scriptURL = 'https://script.google.com/macros/s/AKfycbysZo-s5oE2GHhnTTJYOTlttYKO6yvVE2bG7nYUMkCJyeTXypyJUFeJjQxMxL1ClL7H/exec'; 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwO9o0-dVWH3Obuq__GoIO8_HdBP7avzDJaPoQY6QSL506dq8kIAN16-1B9lDk92C1X/exec'; 
 const form = document.forms['google-sheet-form'];
 const btn = document.getElementById('submitBtn');
 const msg = document.getElementById('formMsg');
